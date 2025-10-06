5348bbb80175d45de4952b2203f7edc665e551b0
Dawaman43 <dawitworkujima@gmail.com>
2025-10-06 07:05:27 +0300

    added header component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/layout/header.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Header />
         <App />
       </ThemeProvider>
     </BrowserRouter>
